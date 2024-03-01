7142667bd2a3d45c15e974edc07973579805cef7
manto <m.antonova.v@gmail.com>
2024-03-01 21:30:35 +0500

    доработала функции в channel. в main выдает ожидаемый ответ

--- src/channel.py
+++ src/channel.py
@@ -5,34 +5,16 @@ from googleapiclient.discovery import build
 # создание апи ключа
 api_key: str = os.getenv('YT_API_KEY')
 
-# создаем переменную для получения данных с ютуб
-youtube = build('youtube', 'v3', developerKey=api_key)
-
-url = 'https://developers.google.com/youtube/v3/docs/channels/list'
-channel_id = 'UCDBOWnTdpOOD-hoTcBGTkmQ'
-ch_request = youtube.channels().list(id=channel_id, part='snippet,statistics')
-ch_response = ch_request.execute()
-
-
-def printj(dict_to_print: dict) -> None:
-    """Выводит словарь в json-подобном удобном формате с отступами"""
-    print(json.dumps(dict_to_print, indent=2, ensure_ascii=False))
-
-
-print(ch_response)
 
 class Channel:
     """Класс для ютуб-канала"""
 
     def __init__(self, channel_id) -> None:
         """Экземпляр инициализируется id канала. Дальше все данные будут подтягиваться по API."""
-        self.channel_id = channel_id
+        self.channel_id = channel_id  # id каннала
+        self.youtube = build('youtube', 'v3', developerKey=api_key)
 
-
-    def print_info(self) -> None:
+    def print_info(self):
         """Выводит в консоль информацию о канале."""
-        channel = youtube.channels().list(id=channel_id, part='snippet,statistics')
-        return printj(channel)
-
-
-
+        channel = self.youtube.channels().list(id=self.channel_id, part='snippet,statistics').execute()
+        print(json.dumps(channel, indent=2, ensure_ascii=False))
